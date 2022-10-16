4351f739e7432c67643113376d3b48512f76fa5e
Valentin447 <valentin@myttk.ru>
2022-10-16 23:20:47 +0700

    Add solution Task68

--- Program.cs
+++ Program.cs
@@ -7,16 +7,16 @@
     Console.WriteLine("Введите значение N: ");
     int n = Convert.ToInt32(Console.ReadLine());
 
-    
+
     if (m < n)
     {
         Console.Write($"Числа кратные 3 от {m} до {n}: ");
-        MethodForTask64(m, n);
+        PrintNumbers(m, n);
     }
     else
     {
         Console.Write($"Числа кратные 3 от {n} до {m}: ");
-        MethodForTask64(n, m);
+        PrintNumbers(n, m);
     }
 }
 void Task66()
@@ -41,31 +41,51 @@ void Task68()
 {
     // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+    Console.WriteLine("Введите значение M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
 }
 
-void MethodForTask64(int min, int max)
+void PrintNumbers(int min, int max)
 {
-    if(min >= max) return;
+    if (min > max) return;
     if (min % 3 == 0)
     {
         Console.Write(min + ", ");
     }
     min++;
-    MethodForTask64(min, max);
+    PrintNumbers(min, max);
 }
 int GetSum(int min, int max, int sum = 0)
 {
-    
+
     if (min > max)
     {
         return sum;
     }
-    sum+= min;
+    sum += min;
     min++;
-    //Console.WriteLine(sum);
     return GetSum(min, max, sum);
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
 }
 
 //Task64();
-Task66();
-//Task68();
+//Task66();
+Task68();
